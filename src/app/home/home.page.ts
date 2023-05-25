0d14267e40374370655064458aaee206019cc629
Kaike grando <117050470+kaikeBG@users.noreply.github.com>
2023-05-25 12:36:22 +0000

    site responsivo com css provisorio

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
-  constructor() {}
+  calendario = {
+    mes: 'nulo',
+    ano: 2023,
+    foto: "https://images.pexels.com/photos/8646391/pexels-photo-8646391.jpeg",
+    dias_da_semana: ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab'],
+    dias_da_semana_m: ['Dm', 'Sg', 'Tr', 'Qa', 'Qi', 'Sx', 'Sb'],
+    primeira_semana: ["", 1, 2, 3, 4, 5, 6],
+    segunda_semana: [7,8,9,10,11,12,13],
+    terceira_semana: [14,15,16,17,18,19,20],
+    quarta_semana: [21,22,23,24,25,26,27],
+    quinta_semana: [28,29,30,31, '', '',''],
+  }
 }
